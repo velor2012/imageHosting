9819cdb9a362515bcb1b47e8d2eafc88091367fb
velor2012 <38395332+velor2012@users.noreply.github.com>
2023-03-12 10:54:57 +0800

    update image

---
+++ image-to-webp-compressor-main/README.md
@@ -0,0 +1,17 @@
+# image-to-webp-compressor
+Compress  jpg, jpeg or png images that are inside nested folders into webp images.
+
+## Install requirements
+
+```
+pip install -r requirements.txt
+```
+
+## Add the images folder
+Add folder of images or folders containing images.
+
+## Run the program
+Run the python program in terminal or however you like
+```
+python compressor.py 
+```
---
+++ image-to-webp-compressor-main/compressor.py
@@ -0,0 +1,25 @@
+import os
+from PIL import Image
+from pathlib import Path
+
+def all_files(directory):
+    for path, dirs, files in os.walk(directory): # walks through the folder
+        for f in files:
+            yield os.path.join(path, f)
+
+
+files = [f for f in all_files('E:\\新建文件夹\\OneDrive - 同济大学\\github_repos\\blog\\24.网站利用nginx和cf保护后台') # Edit your name
+               if f.endswith(('.png', '.jpg', 'jpeg'))]
+
+def convert_to_webp(source):
+    destination = source.with_suffix(".webp")
+
+    image = Image.open(source)  # Open image
+    image.save(destination, format="webp")  # Convert image to webp
+    return image
+
+
+for file in files:
+    path = Path(file)
+    convert_to_webp(path)
+    # os.remove(path) # removes previous images after conversion
